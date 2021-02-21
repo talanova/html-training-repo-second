3a6f369bf18221197f56ac538e2bd65264952ad3
talanova <talanova@speechpro.com>
2021-02-21 22:00:17 +0300

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,20 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const webpack = require("webpack");
 const path = require("path");
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, "./js/blog.js"),
+    main: path.resolve(__dirname, "./js/index.js"),
+    post: path.resolve(__dirname, "./js/post.js"),
+    archive: path.resolve(__dirname, "./js/archive.js"),
+    feedback: path.resolve(__dirname, "./js/feedback.js"),
   },
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename: "[name].bundle.js",
+    filename: "[name].js",
   },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
@@ -20,7 +24,32 @@ module.exports = {
     port: 9000,
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: "./pages/blog.html" }),
+    new HtmlWebpackPlugin({
+      template: "./pages/post.html",
+      filename: "post.html",
+      chunks: ["post"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./pages/archive.html",
+      filename: "archive.html",
+      chunks: ["archive"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./pages/feedback.html",
+      filename: "feedback.html",
+      chunks: ["feedback"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./pages/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./media/img",
+          to: "media/img",
+        },
+      ],
+    }),
     new CleanWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new BrowserSyncPlugin(
